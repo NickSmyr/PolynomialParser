00f93d7ccaa83f6af844e09767d96ea16dd89863
Nick Smyrnioudis <32795986+NickSmyr@users.noreply.github.com>
2018-06-01 20:05:29 +0300

    -Basic methods but errors

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 public class Polynomial{
 	private ArrayList<Term> terms;
 	private Polynomial(){
@@ -11,28 +12,35 @@ public class Polynomial{
 	Returns a polynomial parsed from a given string
 	@param input The String 
 	*/
-	public static Polynomial parsePolynomial(String input){
-		input = removeWhiteSpace(input);
+	public static Polynomial parsePolynomial(String input) throws InputMismatchException{
+		ArrayList<Term> terms = new ArrayList<Term>();
 		for(int i=0;i<input.length();i++){//Loops through every character
-			while(input.charAt(i) != '+' &&  input.charAt(i) != '-'){ //Until we find the next term
-				
+			String termData = "";
+			
+			while(i <input.length() && input.charAt(i) != '+' &&  input.charAt(i) != '-'){ //Until we find the next term
+				termData += input.charAt(i);
+				i++;
 			}
-		}	
+			terms.add(Term.parseTerm(termData));
+			
+		}
+		return new Polynomial(terms);
 	}
 	
 	/*
 	Returns string representation of the polynomial
 	*/
 	public String toString(){
-		String res;
+		String res = "";
 		for(Term term: terms) res += term.toString();
-		return res
+		return res;
 	}
 	//Helper methods
 	public static String removeWhiteSpace(String in){
 		in = in.trim();
 		String result = "";
-		for(Char ch : in){
+		for(int i =0 ; i<in.length() ; i++){
+			char ch = in.charAt(i);
 			if(ch != ' ' && ch != '\t'){
 				result += ch;
 			}
--- src/Term.java
+++ src/Term.java
@@ -1,17 +1,17 @@
 import java.util.InputMismatchException;
 public class Term {
 	/*
-	A term in the form of  a * (x ^ b)
+	A term in the form of  a * (x ^ b)  Parenthesis required
 	*/
 	private double coefficient;
 	private int power;
 	public Term(double coefficient,int power){
-		this.coefficient = coeffticient;
+		this.coefficient = coefficient;
 		this.power = power;
 	}
 	//Setters
 	public void setCoefficient(double co){this.coefficient = co;}
-	public void setPower(double co){this.coefficient = co;}
+	public void setPower(int pow){this.power = pow;}
 	//Getters
 	public double getCoefficient(){return coefficient;}
 	public int getPower(){return power;}
@@ -22,7 +22,7 @@ public class Term {
 	}
 	//toString
 	public String toString(){
-		return String.format("%.2f(x^%d)",coefficient,power);
+		return String.format("%.2f*(x^%d)",coefficient,power);
 	}
 	/*
 	Returns a polynomial parsed from a given string
@@ -30,9 +30,28 @@ public class Term {
 	@return Term parsed
 	*/
 	public static Term parseTerm(String input) throws InputMismatchException{
+		double coefficient = -1;
+		int power = -1;
 		for(int i=0;i<input.length();i++){
-			if(input.startsWith("+") || input.startsWith("-")){ //Coefficient is specified first
+			
+			if(input.startsWith("+") || input.startsWith("-")||Character.isDigit(input.charAt(i))){//Coefficient is specified 
+				String coeff = "";
+				for(;i<input.length() && input.charAt(i) != '*';i++ ){ //Loops as long as the user hasn't entered *
+					coeff += input.charAt(i);
+				}
+				coefficient = Double.parseDouble(coeff); //saving coefficient
 			}
+			if(i<input.length() && input.charAt(i) == '('){
+				String pow="";
+				for(; i<input.length() && input.charAt(i) != ')' ;i++){ //Loops until we find the power of the number x 
+					if(Character.isDigit(input.charAt(i))) pow += input.charAt(i);
+				}
+				power = Integer.parseInt(pow); //Saving power
+				
+			}		
 		}
+		//Data check
+		if(power == -1 || coefficient == -1) throw new InputMismatchException();
+		else return new Term(coefficient,power);
 	}
 }
